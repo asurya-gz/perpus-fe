5d2fca6ef32d05cdadce92e25b82a5259aaaad9f
agsry <agungkdp208@gmail.com>
2024-10-24 17:59:25 +0700

    pppp

--- src/app/page/CaraKerja.js
+++ src/app/page/CaraKerja.js
@@ -9,21 +9,21 @@ export default function CaraKerja() {
           <h3 className="text-3xl font-bold mb-8">Cara Kerja</h3>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div className="flex flex-col items-center justify-center">
-              <Image src="/ppp.png" alt="Step 1" width={300} height={300} />
+              <Image src="/logo.png" alt="Step 1" width={300} height={300} />
               <h4 className="text-xl font-semibold mt-4">1. Daftar Akun</h4>
               <p className="text-gray-700">
                 Buat akun dan masuk ke dalam sistem.
               </p>
             </div>
             <div className="flex flex-col items-center justify-center">
-              <Image src="/ppp.png" alt="Step 2" width={300} height={300} />
+              <Image src="/logo.png" alt="Step 2" width={300} height={300} />
               <h4 className="text-xl font-semibold mt-4">2. Pilih Ruangan</h4>
               <p className="text-gray-700">
                 Pilih ruangan yang tersedia sesuai dengan kebutuhan Anda.
               </p>
             </div>
             <div className="flex flex-col items-center justify-center">
-              <Image src="/ppp.png" alt="Step 3" width={300} height={300} />
+              <Image src="/logo.png" alt="Step 3" width={300} height={300} />
               <h4 className="text-xl font-semibold mt-4">
                 3. Konfirmasi Peminjaman
               </h4>

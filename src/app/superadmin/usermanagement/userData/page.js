dbb6c2ab78d2fcef4844a1602e1bac7d8831c4d1
agsry <agungkdp208@gmail.com>
2024-10-24 17:13:23 +0700

    nnn

--- src/app/superadmin/usermanagement/userData/page.js
+++ src/app/superadmin/usermanagement/userData/page.js
@@ -1,15 +1,17 @@
-export const initialUsers = [
-    { id: 1, username: "Agung Surya", email: "agung@example.com", role: "Admin" },
-    {
-      id: 2,
-      username: "Dewi Sartika",
-      email: "dewi@example.com",
-      role: "Editor",
-    },
-    {
-      id: 3,
-      username: "Budi Santoso",
-      email: "budi@example.com",
-      role: "Viewer",
-    },
-  ];
+const initialUsers = [
+  { id: 1, username: "Agung Surya", email: "agung@example.com", role: "Admin" },
+  {
+    id: 2,
+    username: "Dewi Sartika",
+    email: "dewi@example.com",
+    role: "Editor",
+  },
+  {
+    id: 3,
+    username: "Budi Santoso",
+    email: "budi@example.com",
+    role: "Viewer",
+  },
+];
+
+export default initialUsers;
